1c11289a8b2a184267ee2c38962016c2396bf672
eunji04 <127325105+eunji04@users.noreply.github.com>
2024-04-09 16:26:03 +0900

    [Silver II] Title: 결혼식, Time: 184 ms, Memory: 18008 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -1,14 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static int N, cnt;
-	static int[][] arr;
+	static int N;
+	
+	static int[][] graph;
 	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
@@ -16,8 +17,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N+1][N+1];
-		visited = new boolean[N+1];
+		
+		graph = new int[N+1][N+1];
 		
 		int M = Integer.parseInt(br.readLine());
 		for(int i=0; i<M; i++) {
@@ -26,40 +27,31 @@ public class Main {
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			
-			arr[start][end] =1;
-			arr[end][start] =1;
+			graph[start][end] =1;
+			graph[end][start] =1;
 		}
 		
-		visited[1] = true;
+		visited = new boolean[N+1];
 		
-		Queue<Integer> que = new ArrayDeque<>();
-		for(int j=1; j<=N; j++) {
-			if(arr[1][j]!=0) {
-				visited[j] = true;
-				que.offer(j);
+		List<Integer> list = new ArrayList<>();
+		for(int i=2; i<=N; i++) {
+			if(graph[1][i]!=0) {
+				visited[i] = true;
+				list.add(i);
 			}
 		}
 		
-		while(!que.isEmpty()) {
-			int n = que.poll();
-			check(n);
+		for(int x : list) {
+			for(int i=2; i<=N; i++) {
+				if(graph[x][i]!=0 && !visited[i]) visited[i]=true;
+			}
 		}
 		
+		int cnt =0;
 		for(int i=2; i<=N; i++) {
 			if(visited[i]) cnt+=1;
 		}
 		System.out.println(cnt);
-		
-	}
-	
-	static void check(int n) {
-		
-		for(int j=1; j<=N; j++) {
-			if(arr[n][j]!=0 && !visited[j]) {
-				visited[j] = true; 
-			}
-		}
-		
 	}
 	
 }
